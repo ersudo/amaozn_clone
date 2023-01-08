1f73f353b6645371efccfecb7c8bc150d83825a4
ersudo <ervin608@yahoo.com>
2023-01-08 20:20:35 +0900

    update file

--- src/Components/DisplayComtent/RightSide/Product.js
+++ src/Components/DisplayComtent/RightSide/Product.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import "./RightSide.css"
+import Rating from '@mui/material/Rating'
+import getSymbolFromCurrency from 'currency-symbol-map'
 export default function Product(props) {
   return (
     <div className="product">
         <div className="product__image">
-            <div><img src={props.image} height={"300px"}/> 
+            <div><img src={props.definition.image} height={"300px"}/> 
             </div>
         </div>
         <div className="product__name">
-            {props.name}    
+            {props.definition.name}    
         </div>
         <div className="product__rating">
-            {props.rating}
+            <Rating name="read-only" value="4" style={{fontSize:"20px"}} readOnly />
+            {props.definition.rating}
         </div>
         <div className="product__price">
-            {props.price}
+            {getSymbolFromCurrency('USD')}
+            {props.definition.price}
         </div>
     </div>
 
--- src/Components/DisplayComtent/RightSide/RightSidePanel.js
+++ src/Components/DisplayComtent/RightSide/RightSidePanel.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from './Product'
 import "./RightSide.css"
 export default function RightSidePanel(props) {
+  
+  const [listOfProduct,setListOfProducts] = useState([])
+  
+  useEffect(() =>{
+      let list = [
+        {name: "iPhone10", rating: "338,195", price:"58,399" , image:"https://ik.imagekit.io/amazon1230/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259746"},
+        {name: "iPhone11", rating: "338,195", price : "58,399", image:"https://ik.imagekit.io/amazon1230/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259877"},
+        {name: "iPhone12", rating : "338,195", price : "58,399", image:"https://ik.imagekit.io/amazon1230/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259867"},
+        {name: "iPhone13", rating : "338,195", price : "58,399", image:"https://ik.imagekit.io/amazon1230/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259807"},
+      ]
+      setListOfProducts(list)
+  })  
+
   return (
     <div className="RightSize__main">
-        <Product name="New iPhone 11pro(64GB) - Green" price="58,399" rating="338,898" image="https://ik.imagekit.io/amazon1230/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259746"/>
+        {/* <Product name="New iPhone 11pro(64GB) - Green" price="58,399" rating="338,898" image="https://ik.imagekit.io/amazon1230/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672395259746"/>
         <Product/>
         <Product/>
         <Product/>
         <Product/>
-        <Product/>
+        <Product/> */}
+        {
+          listOfProduct.map( (item) =>(
+            // <Product rating ={item.rating} name = {item.name} image={item.image} price={item.price}/>
+            <Product definition={item}/>
+          ))
+        }
     </div>
   )
 }
